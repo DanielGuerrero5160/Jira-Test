c449cd6429f899b5ae7743b568b9b10ca06de4bf
DanielGuerrero5160 <daniel.guerrero5160@gmail.com>
2023-09-14 23:35:37 -0400

    Fuck you one thousand times

--- src/test/java/LetsTestThis.java
+++ src/test/java/LetsTestThis.java
@@ -1,2 +1,9 @@
 public class LetsTestThis {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Fuck you");
+        }
+    }
 }
